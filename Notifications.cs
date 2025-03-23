f283d4237bb4097c1af6fd097f2f9a5c3d6aa6ab
DemonExposer <kbdmns@gmail.com>
2025-03-23 23:11:20 +0100

    Added notifications for Linux

--- Notifications.cs
+++ Notifications.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace OsNotifications;
 
@@ -9,9 +10,28 @@ public partial class Notifications {
 	private static partial void showNotification([MarshalAs(UnmanagedType.LPStr)] string identifier, [MarshalAs(UnmanagedType.LPStr)] string title, [MarshalAs(UnmanagedType.LPStr)] string subtitle, [MarshalAs(UnmanagedType.LPStr)] string informativeText);
 	
 	public static void ShowNotification(string title, string message = "", string informativeText = "") {
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			ShowNotificationLinux(title, message);
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			ShowNotificationMac(title, message, informativeText);
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			ShowNotificationWindows(title, message);
+		else
+			throw new PlatformNotSupportedException("Notifications are only supported on Linux, MacOS and Windows");
+	}
+
+	private static void ShowNotificationLinux(string title, string message) {
+		Process.Start("notify-send", $"\"{title}\" \"{message}\"");
+	}
+
+	private static void ShowNotificationMac(string title, string message, string informativeText) {
 		if (BundleIdentifier == null)
 			throw new InvalidOperationException("BundleIdentifier must be set on MacOS before calling ShowNotification. If it is set to a non-existing identifier, no notification will be shown!");
 		
 		showNotification(BundleIdentifier, title, message, informativeText);
+	}
+
+	private static void ShowNotificationWindows(string title, string message) {
+		
 	}
 }
