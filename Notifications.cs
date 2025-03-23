39d8cb43668198289f489db69550cd717e84a1a1
DemonExposer <56930597+DemonExposer@users.noreply.github.com>
2025-03-23 16:07:54 +0100

    Notifications for MacOS

---
+++ Notifications.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OsNotifications;
+
+public partial class Notifications {
+	public static string? BundleIdentifier = null;
+	
+	[LibraryImport("macNotification.dylib")]
+	private static partial void showNotification([MarshalAs(UnmanagedType.LPStr)] string identifier, [MarshalAs(UnmanagedType.LPStr)] string title, [MarshalAs(UnmanagedType.LPStr)] string subtitle, [MarshalAs(UnmanagedType.LPStr)] string informativeText);
+	
+	public static void ShowNotification(string title, string message = "", string informativeText = "") {
+		if (BundleIdentifier == null)
+			throw new InvalidOperationException("BundleIdentifier must be set on MacOS before calling ShowNotification. If it is set to a non-existing identifier, no notification will be shown!");
+		
+		showNotification(BundleIdentifier, title, message, informativeText);
+	}
+}
---
+++ macNotification.m
@@ -0,0 +1,83 @@
+// Code taken from https://github.com/norio-nomura/usernotification
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+#pragma mark - Swizzle NSBundle
+
+NSString *fakeBundleIdentifier = nil;
+
+@implementation NSBundle(swizle)
+
+- (NSString *)__bundleIdentifier {
+    if (self == [NSBundle mainBundle]) {
+        return fakeBundleIdentifier ? fakeBundleIdentifier : @"com.apple.finder";
+    } else {
+        return [self __bundleIdentifier];
+    }
+}
+
+@end
+
+BOOL installNSBundleHook() {
+    Class class = objc_getClass("NSBundle");
+    if (class) {
+        method_exchangeImplementations(class_getInstanceMethod(class, @selector(bundleIdentifier)),
+                                       class_getInstanceMethod(class, @selector(__bundleIdentifier)));
+        return YES;
+    }
+    return NO;
+}
+
+
+#pragma mark - NotificationCenterDelegate
+
+@interface NotificationCenterDelegate : NSObject<NSUserNotificationCenterDelegate>
+
+@property (nonatomic, assign) BOOL keepRunning;
+
+@end
+
+@implementation NotificationCenterDelegate
+
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center didDeliverNotification:(NSUserNotification *)notification {
+    self.keepRunning = NO;
+}
+
+@end
+
+
+#pragma mark -
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+void showNotification(char *identifier, char *title, char *subtitle, char *informativeText) {
+    if (installNSBundleHook()) {
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        
+        fakeBundleIdentifier = [NSString stringWithUTF8String:identifier];
+        
+        NSUserNotificationCenter *nc = [NSUserNotificationCenter defaultUserNotificationCenter];
+        NotificationCenterDelegate *ncDelegate = [[NotificationCenterDelegate alloc]init];
+        ncDelegate.keepRunning = YES;
+        nc.delegate = ncDelegate;
+        
+        NSUserNotification *note = [[NSUserNotification alloc] init];
+        note.title = [NSString stringWithUTF8String:title];
+        note.subtitle = [NSString stringWithUTF8String:subtitle];
+        note.informativeText = [NSString stringWithUTF8String:informativeText];
+        
+        [nc deliverNotification:note];
+        
+        while (ncDelegate.keepRunning) {
+            [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+        }
+    }
+}
+    
+#ifdef __cplusplus
+}
+#endif
+    
